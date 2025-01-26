00e7a81bbc355d8568ad706a879e66f8284606fd
Miles8888 <168711948+Miles8888@users.noreply.github.com>
2025-01-26 13:41:43 -0500

    Add files via upload

---
+++ 2.4-name_cases.py
@@ -0,0 +1,7 @@
+fullname = "yawning cat"
+fullname2 = "YAWNING CAT"
+print(fullname.upper())
+
+print(fullname2.lower())
+
+print(fullname.title())
---
+++ 2.8-number_eight.py
@@ -0,0 +1,22 @@
+
+
+# addition
+print(1+7)
+
+#multiplication
+
+print(2*2*2)
+
+#division
+
+print(16/2)
+
+#division
+
+print(16/2)
+
+#roots/exponents
+
+print(64**0.5)
+
+print(2**3)
