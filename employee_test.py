ff26d134e8e64e399c7105ed8edb1286fe25fdb2
Miles8888 <168711948+Miles8888@users.noreply.github.com>
2025-04-13 20:28:08 -0400

    Add files via upload

---
+++ employee_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from employee import Employee
+
+@pytest.fixture
+def michael():
+    """Fixture to create an example employee."""
+    return Employee('michael', 'woods', 75000)
+
+def test_wage_increase(michael):
+    """Test whether raising the default wage by its default amount works properly."""
+    michael.give_raise()
+    assert michael.salary == 80000
+
+def test_wage_increase_custom(michael):
+    """Test whether raising the default wage by a custom amount works properly."""
+    michael.give_raise(15000)
+    assert michael.salary == 80000
